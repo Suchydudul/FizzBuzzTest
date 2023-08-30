854d70b07926fbed660f2fde27a775b4a3198654
Suchydudul <60809305+Suchydudul@users.noreply.github.com>
2023-08-30 11:59:45 +0200

    Add files via upload

---
+++ fizzbuzz/fizzbuzz/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 100; x++)
+            {
+                string z = "";
+                if (x % 3 == 0) z += "Fizz";
+                if (x % 5 == 0) z += "Buzz";
+                if (z.Length == 0) z += x;
+                Console.WriteLine(z);
+            }
+        }
+    }
+}
